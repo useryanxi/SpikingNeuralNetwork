a3dde471fea809bda1f52bdbffce27dfc3db9806
Laroustine <47363827+Laroustine@users.noreply.github.com>
2023-03-29 09:53:30 +0200

    [UPD] Profiling

--- test3.py
+++ test3.py
@@ -6,8 +6,12 @@ import imageio.v2 as imageio
 import os
 from rf import rf
 from encode import encode
+import time
 
+img_time = True
+read_time = True
 
+t_begin = time.time()
 net = SNN(784, [28, 28, 10, 10], 10, LeakyIntegrateAndFireNeuron, STDP())
 
 counter = int(input("How many loops : "))
@@ -15,6 +19,7 @@ img_num = int(input("How many img : "))
 T = int(input("Time scale : "))
 
 # Train
+t_train_b = time.time()
 path = r"./mnist_png/training"
 for _ in range(counter):
     for folder in os.listdir(path):
@@ -23,13 +28,26 @@ for _ in range(counter):
         print(f"- Start : '{folder}' with {n_files} files")
         for i, img in enumerate(files):
             img_path = os.path.join(os.path.join(path, folder), img)
+            if img_time:
+                t_conv = time.time()
             train = np.array(encode(rf(imageio.imread(img_path)), T=T))
+            if img_time:
+                img_time = False
+                print(f"Conversion to spike in {time.time() - t_conv}s")
+            if read_time:
+                t_read = time.time()
             for t in range(len(train[0])):
                 net.solve(list(train[:, t]))
+            if read_time:
+                read_time = False
+                print(f"Reading Spikes in {time.time() - t_read}s")
             if i % (n_files / 10) == int(n_files / 10) - 1:
                 print(f"Done at {i * 100 / n_files:.2f}%")
+print(f"Trained in {time.time() - t_train_b}s")
 
 # Test
+read_time = True
+t_test_b = time.time()
 path = r"./mnist_png/testing"
 for folder in os.listdir(path):
     files = os.listdir(os.path.join(path, folder))[:img_num]
@@ -38,8 +56,12 @@ for folder in os.listdir(path):
     for i, img in enumerate(files):
         img_path = os.path.join(os.path.join(path, folder), img)
         train = np.array(encode(rf(imageio.imread(img_path)), T=T))
+        if read_time:
+            t_read = time.time()
         for t in range(len(train[0])):
             print(net.test(list(train[:, t])))
-        print(10 * "*")
-        if i % (n_files / 10) == int(n_files / 10) - 1:
-            print(f"Done at {i * 100 / n_files:.2f}%")
+        if read_time:
+            read_time = False
+            print(f"Load tested image in {time.time() - t_read}s")
+print(f"Tested in {time.time() - t_test_b}s")
+print(f"Done in {time.time() - t_begin}s")
